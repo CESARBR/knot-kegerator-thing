6bdc738d0659ba73583d663b3dabc055c820ccf4
Vitor Barros <vba@cesar.org.br>
2018-01-30 10:32:51 -0300

    Add initial KNoT Kegerator file

---
+++ KNoTKegerator/KNoTKegerator.ino
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include <KNoTThing.h>
+
+KNoTThing thing;
+
+void setup(void)
+{
+	Serial.begin(115200);
+
+	thing.init("KNoTKegerator");
+
+	Serial.println(F("KNoT Kegerator"));
+}
+
+void loop(void)
+{
+	thing.run();
+}
